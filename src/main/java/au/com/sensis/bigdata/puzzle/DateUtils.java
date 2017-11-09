c6540c947568c7770f175bfda43a4e3df4123383
Luke F <luke.feng@sensis.com.au>
2017-11-09 11:20:41 +1100

    Added TimeUnit usage example

--- src/main/java/au/com/sensis/bigdata/puzzle/DateUtils.java
+++ src/main/java/au/com/sensis/bigdata/puzzle/DateUtils.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.time.DateFormatUtils;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class DateUtils {
 
@@ -41,6 +42,19 @@ public class DateUtils {
         }
     }
 
+    public static long getDaysInBetween(Integer dateInt) {
+        SimpleDateFormat format = new SimpleDateFormat(COMPRESSED_YEAR_MONTH_DAY);
+
+        Date start = null;
+        try {
+            start = format.parse(String.valueOf(dateInt));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return TimeUnit.DAYS.convert(new Date().getTime() - start.getTime(), TimeUnit.MILLISECONDS);
+    }
+
     public static void main(String[] args) {
 
         int d = Integer.valueOf(DateFormatUtils.format(new Date(), COMPRESSED_YEAR_MONTH_DAY));
@@ -59,6 +73,9 @@ public class DateUtils {
 
         System.out.println("Last month first day: " + getFirstDayOfMonth(d));
         System.out.println("Last month last day: " + getLastDayOfMonth(d));
+
+        // how many days before today
+        System.out.println("How many days before today? " + getDaysInBetween(20171107));
 
     }
 }
